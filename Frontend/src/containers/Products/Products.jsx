d1b8b4027c22b149e4061cbccfa40ceb225895ae
rithik <brithik3@gmail.com>
2024-03-20 20:57:41 +0530

    updates

--- Frontend/src/containers/Products/Products.jsx
+++ Frontend/src/containers/Products/Products.jsx
@@ -5,12 +5,9 @@ const Products = () => {
   return (
     <>
       <Nav />
-<<<<<<< HEAD
       <div>
         <h1>Products</h1>
       </div>
-=======
->>>>>>> 24c207691f70ef44c655fceaf0a177c5c4a1cd62
     </>
   );
 };
